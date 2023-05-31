80a643d05b6c97c9272d908c481bded2a21e8966
N3xou <47154462+N3xou@users.noreply.github.com>
2023-05-31 15:04:40 +0200

    1.1

--- Server/Controllers/HomeController.cs
+++ Server/Controllers/HomeController.cs
@@ -1,19 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BlazorProject.Shared;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BlazorProject.Server.Controllers
 {
 	public class HomeController : Controller
 	{
+		private readonly QueueService queueService;
+		public HomeController(QueueService queueService) 
+		{
+			this.queueService = queueService;
+		}
 		public IActionResult Index()
 		{
-			Response.Cookies.Append("ID", "1", new CookieOptions
+			// Read the ID cookie
+			string cookieValue = Request.Cookies["ID"];
+
+			// Check if the cookie value is null or empty
+			if (string.IsNullOrEmpty(cookieValue))
 			{
-				Expires = DateTime.Now.AddHours(5),
-			});
+				// Generate a unique ID
+				int uniqueId = GenerateUniqueId();
+				int queueNumber = GetQueueNumber();
+
+				// Create a new cookie with the unique ID
+				Response.Cookies.Append("ID", uniqueId.ToString(), new CookieOptions
+				{
+					Expires = DateTime.Now.AddHours(5),
+				});
+
+				// Add the unique ID to the dictionary
+				queueService.AddToQueue(uniqueId, queueNumber);
+			}
+
 			return View();
 		}
-		private Dictionary<string, string> _cookies;
-		
+
+		private int GenerateUniqueId()
+		{
+			// Generate a unique ID using a suitable logic, such as using a GUID or a random number generator
+			// For simplicity, let's generate a random number between 1 and 1000
+			Random random = new Random();
+			int uniqueId = random.Next(1, 1001);
+			return uniqueId;
+		}
+		private int GetQueueNumber()
+		{
+			// Calculate the next queue number based on the dictionary's count
+			int queueNumber = queueService.queueDict.Count + 1;
+			return queueNumber;
+		}
+
+
+
 	}
 }
 // test, not working yet
---
+++ Shared/QueueService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorProject.Shared
+{
+	public class QueueService
+	{
+		public Dictionary<int, int> queueDict;
+
+		public QueueService()
+		{
+			queueDict = new Dictionary<int, int>();
+		}
+
+		public void AddToQueue(int key, int value)
+		{
+			queueDict.Add(key, value);
+		}
+
+		// Other methods to interact with the queueDict as needed
+	}
+
+}
